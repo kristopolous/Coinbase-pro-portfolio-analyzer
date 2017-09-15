4db3db9364ca2962db1b572661bebf505389d849
Chris McKenzie <kristopolous@yahoo.com>
2017-09-15 02:23:33 -0700

    showing total balances

--- balances.py
+++ balances.py
@@ -9,6 +9,12 @@ cur_balances = p.returnCompleteBalances()
 all_balances = list([(k, float(v['btcValue']), float(v['available']) + float(v['onOrders'])) for k,v in cur_balances.items() ])
 all_positive = list(filter(lambda x: x[1] > 0, all_balances))
 in_order = sorted(all_positive, key=itemgetter(1))
+btc_ttl = 0
+
 for k,b,v in in_order:
     if b > 0:
-        print("{:6} {:.8f} {:14.8f} {:8.2f}".format( k, b, v, b * lib.btc_price() ))
+        btc_ttl += b
+        print("{:6} {:.8f} {:8.2f} {:14.8f}".format( k, b, b * lib.btc_price(), v ))
+
+print("--------------------------")
+print("{:6} {:.8f} {:8.2f}".format( 'all', btc_ttl, btc_ttl * lib.btc_price() ))
