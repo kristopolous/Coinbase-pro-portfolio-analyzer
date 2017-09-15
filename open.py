27e0472fd03b122e668b378b38b3add6822e5760
Chris McKenzie <kristopolous@yahoo.com>
2017-09-15 01:03:15 -0700

    Better output and caching for open orders

--- open.py
+++ open.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import lib
+import json
 import sys
 
 currency = False
@@ -9,12 +10,19 @@ if len(sys.argv) > 1:
 def show(what, l):
     subset = list(filter(lambda x: x['type'] == what, l))
     if subset:
-        print(what, sum([float(i['total']) for i in subset]))
+        print(" {:4} {:.8f}".format(what, sum([float(i['total']) for i in subset])))
         for i in subset:
-            print(i['date'], i['rate'], i['total'])
+            print("  {} {} {} {}".format(i['orderNumber'], i['date'], i['rate'], i['total']))
 
-p = lib.polo_connect()
-plist = p.returnOpenOrders()
+if lib.need_to_get('cache/open', expiry=240):
+   with open('cache/open', 'w') as cache:
+       p = lib.polo_connect()
+       plist = p.returnOpenOrders()
+       json.dump(plist, cache)
+
+with open('cache/open') as json_data:
+    plist = json.load(json_data)
+
 for k,v in plist.items():
     if ((currency and currency == k) or not currency) and len(v):
         print(k)
