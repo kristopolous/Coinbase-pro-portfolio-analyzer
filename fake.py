a7d881dd3917d9ba10739a7054278ae405edf0d1
Chris McKenzie <kristopolous@yahoo.com>
2017-09-21 18:34:38 -0700

    adding back some fake routines to the lib

--- fake.py
+++ fake.py
@@ -103,3 +103,16 @@ def connect():
     return singleton
 
     
+def trade(what, tradeList, amount, price):
+    tradeList.append({
+      'rate': price,
+      'amount': amount,
+      'total': price * amount,
+      'type': what
+    })
+
+def sell(tradeList, amount, price):
+    trade('sell', tradeList, amount, price)
+
+def buy(tradeList, amount, price):
+    trade('buy', tradeList, amount, price)
