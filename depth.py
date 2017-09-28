b5c08b071dde1d32e5c95583f89b95aa422e3505
Chris McKenzie <kristopolous@yahoo.com>
2017-09-28 16:11:04 -0700

    updating the depth

--- depth.py
+++ depth.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import lib
-from pprint import pprint
+import json
+import time
 
 cur = lib.getCurrency()
 p = lib.connect()
-pprint(p.returnOrderBook(cur))
+snap = time.time()
+book = p.returnOrderBook(depth=40)
+book['at'] = snap
+print(json.dumps(book))
