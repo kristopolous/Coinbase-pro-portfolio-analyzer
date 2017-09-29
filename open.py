0e208ab10a4080d7c27f72514167567e1cef979c
Chris McKenzie <kristopolous@yahoo.com>
2017-09-29 00:53:55 -0700

    color coordinating the open sell offers based on p&l margins

--- open.py
+++ open.py
@@ -6,17 +6,29 @@ currency = False
 if len(sys.argv) > 1:
     currency = 'BTC_{}'.format(sys.argv[1].upper())
 
-def show(what, l):
+def show(what, l, anal = False):
     subset = list(filter(lambda x: x['type'] == what, l))
     if subset:
         lib.bprint(" {:4} {:.8f}".format(what, sum([float(i['total']) for i in subset])))
         for i in subset:
-            lib.bprint("  {} {} {} {}".format(i['orderNumber'], i['date'], i['rate'], i['total']))
+            i['rate'] = float(i['rate'])
+            color = '0'
+            if what == 'sell':
+                if i['rate'] < anal['buyAvg']:
+                    color='91'
+                elif i['rate'] < anal['break']:
+                    color='35'
+                elif i['rate'] > max(anal['break'], anal['buyAvg'], anal['sellAvg']):
+                    color='32'
+
+            lib.bprint("\x1b[{}m  {} {} {:.8f} {}\x1b[0m".format(color, i['orderNumber'], i['date'], i['rate'], i['total']))
 
 plist = lib.returnOpenOrders()
+history = lib.tradeHistory()
 
 for k,v in plist.items():
     if ((currency and currency == k) or not currency) and len(v):
         print(k)
         show('buy', v)
-        show('sell', v)
+        anal = lib.analyze(history[k])
+        show('sell', v, anal)
