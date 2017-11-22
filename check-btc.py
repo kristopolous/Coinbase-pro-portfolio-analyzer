b649b151b18010ad734e3e4827d5ef6e14fefe8b
Chris McKenzie <kristopolous@yahoo.com>
2017-11-21 16:42:16 -0800

    adding a way to notify on large move events

--- check-btc.py
+++ check-btc.py
@@ -7,11 +7,11 @@ import requests
 current = lib.btc_price(force=True)
 name = 'btc-limits.json'
 
-def notify(last, current, history):
+def notify(last, current, history, direction):
     history = [str(x) for x in history]
     key = secret.mailgun[0]
     request_url = "%s/%s" % (secret.mailgun[1].strip('/'), 'messages')
-    message = "BTC moved from {} to {}".format(last, current)
+    message = "BTC {} from {} to {}".format(direction, last, current)
     request = requests.post(request_url, auth=('api', key), data={
        'from': secret.email[0],
        'to': secret.email[1],
@@ -19,16 +19,31 @@ def notify(last, current, history):
        'text': "\n".join(history),
        'html': "<br>".join(history)
     })
+    print(message)
 
 with open(name, 'r') as json_data:
     d = json.load(json_data)
     range_list = d['range']
     last = d['last']
 
+    direction = False
     for cutoff in range_list:
-        if (current < cutoff and last > cutoff) or (current > cutoff and last < cutoff):
-            notify(last, current, d['history'])
+        if current < cutoff and last > cutoff:
+            direction = "down"
+
+        if current > cutoff and last < cutoff:
+            direction = "up"
+
+        if direction:
             break
+
+    perc = abs(last - current) / last
+    if not direction and perc > 0.03:
+        word = "up" if current > last else "down"
+        direction = "{} {}%".format(word, perc * 100)
+
+    if direction:
+        notify(last, current, d['history'], direction)
 
     d['history'].append(current)
     d['history'] = d['history'][-20:]
