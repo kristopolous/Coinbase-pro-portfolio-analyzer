1d4746a0a30324d926dc4b408c66148e9257dd23
Chris McKenzie <kristopolous@yahoo.com>
2017-12-19 20:57:57 -0800

    fixing a bug in the bot

--- bot.py
+++ bot.py
@@ -7,12 +7,14 @@ p = Poloniex(*secret.token)
 
 margin = 0.015
 min = 10015
+
 while True:
     all_prices = lib.returnTicker(forceUpdate = True)
     all_trades = lib.tradeHistory('all', forceUpdate=True)
     cur_balances = {k: v for k, v in lib.returnCompleteBalances().items() if v['btcValue'] > 0.00001}
     positive_balances = {k: v['cur'] for k,v in cur_balances.items() }
 
+    tcount = 0
     for k, v in positive_balances.items():
         if k == 'BTC':
             continue
@@ -26,13 +28,15 @@ while True:
             print("SELL {:9} {:.8f} {:.8f}".format(exchange, current['highestBid'], last_rate))
             amount_to_trade = min / int(current['highestBid_orig'].lstrip("0."))
             res = p.sell(currencyPair=exchange, rate=current['highestBid_orig'], amount=amount_to_trade, orderType="fillOrKill")
-            print(res)
+            tcount += 1
 
         elif last_rate * (1 - margin) > current['lowestAsk']:
             print("BUY  {:9} {:.8f} {:.8f}".format(exchange, current['lowestAsk'], last_rate))
             amount_to_trade = min / int(current['lowestAsk_orig'].lstrip("0."))
             res = p.buy(currencyPair=exchange, rate=current['lowestAsk_orig'], amount=amount_to_trade, orderType="fillOrKill")
-            print(res)
+            tcount += 1
 
-    print("no trades")
-    time.sleep(300)
+    if tcount == 0:
+        print("no trades")
+
+    time.sleep(120)
