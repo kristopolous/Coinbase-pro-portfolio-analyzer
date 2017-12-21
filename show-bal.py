c3c25fb0c48d647ef9095c691e4cc4aeb6624115
Chris McKenzie <kristopolous@yahoo.com>
2017-12-21 05:01:21 -0800

    A simple balance shower

---
+++ show-bal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import lib
+from pprint import pprint
+
+all_history = lib.tradeHistory()
+balanceMap = lib.returnCompleteBalances()
+
+for k,v in all_history.items():
+    print(k)
+
+    v = lib.analyze(v, currency=k, brief=True, sort='date')
+    cur = k[4:]
+    if cur in balanceMap:
+        v['bal'] = balanceMap[cur]
+
+    pprint(v)
