4c2c869b093567f8b7212b5ee9ea46af5eadd548
Chris McKenzie <kristopolous@yahoo.com>
2017-12-21 03:51:58 -0800

    more safety measures

--- trade.py
+++ trade.py
@@ -14,7 +14,6 @@ parser.add_argument('-a', "--action", required=True, help="Action, either buy or
 parser.add_argument('-q', "--quantity", default=0, help="Quantity to buy, expressed in btc (also 'min' for minimum amount)")
 parser.add_argument('-r', "--rate", default=None, help="Rate (defaults to market). Also accepts lowest, highest,break, profit, ask, bid, and percent. Also math can be done. Such as profit+0.2%")
 parser.add_argument('-n', "--nofee", action='store_true', help="Try to avoid the higher fee")
-parser.add_argument('-f', "--fast", action='store_true', help="Skip the ceremony and do things quickly")
 args = parser.parse_args()
 
 p = Poloniex(*secret.token)
@@ -34,7 +33,6 @@ if rate:
     rate = rate.replace('_', '0')
 
 action = args.action.lower()
-fast = args.fast
 lowest = False
 spreadThreshold = 0.005
 
@@ -47,7 +45,9 @@ spreadThreshold = 0.005
 approx_btc_usd = lib.btc_price()
 
 # let's set it really low for now. 
-warn_at_usd = 9.50
+warn_at_usd = 12.50
+double_warn_at_usd = 45
+fail_at_usd = 80
 
 def warn(msg):
     lib.bprint("\nWARNING:\n {}\n".format(msg.replace('\n', '\n ')))
@@ -57,10 +57,21 @@ def abort(msg):
     lib.bprint("\nERROR:\n {}\n\n Aborted.".format(msg.replace('\n', '\n ')))
     sys.exit(-1)
 
+if quantity > (fail_at_usd / approx_btc_usd):
+    usd = approx_btc_usd * quantity
+    abort("I'm refusing to trade ${:.2f}".format(usd))
+
 if quantity > (warn_at_usd / approx_btc_usd):
     usd = approx_btc_usd * quantity
     print("Above ${:.2f} warning triggered!".format(warn_at_usd))
-    quantity_confirm = input("You're about to trade ${:.2f}!\nConfirm the quantity in BTC > ".format(usd))
+    quantity_confirm = input("You're about to trade\n\n    {:.2f}\n\nConfirm the quantity in BTC > ".format(usd))
+    if quantity_confirm != args.quantity:
+        abort("Numbers don't match.")
+
+if quantity > (double_warn_at_usd / approx_btc_usd):
+    usd = approx_btc_usd * quantity
+    print("Above ${:.2f} warning triggered!".format(double_warn_at_usd))
+    quantity_confirm = input("NO REALLY LOOK AT THAT FUCKING NUMBER AND TYPE IT AGAIN > ".format(usd))
     if quantity_confirm != args.quantity:
         abort("Numbers don't match.")
 
@@ -70,7 +81,7 @@ if action != 'buy' and action != 'sell':
 print("EXCHANGE {}".format(exchange))
 
 price_pump = 0.00000001
-if not fast or rate is None or rate.find('%') > -1 or re.search('[a-z]', rate):
+if rate is None or rate.find('%') > -1 or re.search('[a-z]', rate):
     priceMap = p.returnTicker()
 
     if exchange not in priceMap:
@@ -157,7 +168,7 @@ if not fast or rate is None or rate.find('%') > -1 or re.search('[a-z]', rate):
             rate *= 1 + perc
         else:
             if perc < 0.5:
-                warn("Requesting a rate {:.0f}% below market".format(100 * (1-perc) ))
+                abort("Requesting a rate {:.0f}% below market".format(100 * (1-perc) ))
             rate *= perc
 
     if args.nofee: 
@@ -179,13 +190,12 @@ if not fast or rate is None or rate.find('%') > -1 or re.search('[a-z]', rate):
 fl_rate = round(float(rate) * 1e8) / 1e8
 lib.bprint("\nComputed\n Rate  {:.8f}\n Quant {:.8f}\n USD   {:.3f} (btc={:.2f})".format(fl_rate, float(quantity), float(quantity) * approx_btc_usd, approx_btc_usd))
 
-if not fast:
-    balanceMap = lib.returnCompleteBalances()
-    row = balanceMap[currency]
+balanceMap = lib.returnCompleteBalances()
+row = balanceMap[currency]
 
-    print("\nBalance:\n BTC   {:>13}\n {:6}{:13.8f}".format(
-        row['btcValue'], currency, float(row['onOrders']) + float(row['available'])
-      ))
+print("\nBalance:\n BTC   {:>13}\n {:6}{:13.8f}".format(
+    row['btcValue'], currency, float(row['onOrders']) + float(row['available'])
+  ))
 
 quantity += .499 * lib.satoshi
 amount_to_trade = quantity / fl_rate
@@ -194,17 +204,16 @@ lib.bprint("\n{}\n   {:12.8f}\n * {:12.8f}BTC\n = {:12.8f}BTC".format(action.upp
 if quantity == 0:
     sys.exit(-1)
 
-if False or not fast:
-    wait = 2
-    print("\nWaiting {} seconds for user abort".format(wait))
-    for i in range(wait, 0, -1):
-        print("...{}".format(i - 1), end='', flush=True)
-        try:
-            time.sleep(1)
-        except: 
-            abort("Trade Halted")
+wait = 2
+print("\nWaiting for user abort".format(wait))
+for i in range(wait, 0, -1):
+    print("...{}".format(i - 1), end='', flush=True)
+    try:
+        time.sleep(1)
+    except: 
+        abort("Trade Halted")
 
-    print("")
+print("")
 
 def buy(exchange, rate, amount_to_trade, args):
     try:
@@ -225,17 +234,15 @@ def buy(exchange, rate, amount_to_trade, args):
 
 
 if action == 'buy':
-    if not fast:
-        if fl_rate > (lowest * 1.2):
-            abort("{:.10f}BTC is the lowest ask.\n{:.10f}BTC is over 20% more than this!".format(lowest, fl_rate))
+    if fl_rate > (ask * 1.2):
+        abort("{:.10f}BTC is the lowest ask.\n{:.10f}BTC is over 20% more than this!".format(lowest, fl_rate))
 
     buy_order = buy(exchange, rate, amount_to_trade, args)
     lib.showTrade(buy_order, exchange, trade_type='buy', rate=rate, amount=amount_to_trade)
 
 elif action == 'sell':
-    if not fast:
-        if fl_rate < (bid * 0.8):
-            abort("{:.10f}BTC is the highest bid.\n{:.10f}BTC is over 20% less than this!".format(lowest, fl_rate))
+    if fl_rate < (bid * 0.8):
+        abort("{:.10f}BTC is the highest bid.\n{:.10f}BTC is over 20% less than this!".format(lowest, fl_rate))
     
    # print("{:.8f} {:.8f} {:.8f}".format(rate, amount_to_trade, rate*amount_to_trade))
     sell_order = p.sell(exchange, rate, amount_to_trade)
