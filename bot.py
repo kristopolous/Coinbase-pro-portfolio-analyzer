98bc24f231401ec4f1db693117c834cf65b7a8b5
Chris McKenzie <kristopolous@yahoo.com>
2017-12-27 19:51:39 -0800

    new strategy outperforms 99.98% of the time.

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ p = Poloniex(*secret.token_old)
 f = open('/dev/null', 'w')
 #sys.stderr = f
 
-margin = 0.0125
-min = 19100
+margin = 0.0117
+unit = 19100
 # .005 is accounted for in the trades.
 frac = 1 + ((margin - 0.005) * 0.92)
 lower = (1 - margin)
@@ -27,29 +27,51 @@ while True:
             continue
 
         exchange = "BTC_{}".format(k)
-        last_trade = all_trades[exchange][-1]
+        backwardList = reversed(all_trades[exchange])
+        last_trade = all_trades[exchange][-1] 
         last_rate = last_trade['rate']
+        sellList = [last_trade['rate']]
+        buyList = [last_trade['rate']]
+        for trade in backwardList:
+            if trade['type'] == 'sell':
+                if len(sellList) < 5:
+                    sellList.append(trade['rate'])
+            else:
+                if len(buyList) < 5:
+                    buyList.append(trade['rate'])
+
+            if len(buyList) == len(sellList) == 5:
+                break
+
+        #print(sellList, buyList)
+        last_buy_max = max(buyList)
+        last_sell_min = min(sellList)
+        #print(last_buy_max, buyList, last_sell_min, sellList)
+        #sys.exit(0)
+        #last_rate = last_trade['rate']
         current = all_prices[exchange]
 
-        if last_rate * upper < current['highestBid']:
-            trade_amount = min * frac
+        if last_buy_max * upper < current['highestBid']:
+            trade_amount = unit * frac
             rate=current['highestBid_orig']
             amount_to_trade = trade_amount / int(rate.lstrip("0."))
             try:
                 res = p.sell(currencyPair=exchange, rate=rate, amount=amount_to_trade, orderType="fillOrKill")
-                lib.showTrade(res, exchange, trade_type='sell', rate=rate, source='bot', amount=amount_to_trade)
+                lib.showTrade(res, exchange, trade_type='sell', rate=rate, source='bot2', amount=amount_to_trade)
             except Exception as ex:
-                print("  SELL {:9}  {:.3f} {}".format(exchange, 100 * (current['highestBid'] / last_rate - 1), ex))
+                print("  SELL {:9}  {:.3f} {}".format(exchange, 100 * (current['highestBid'] / last_buy_max - 1), ex))
 
 
-        elif last_rate * lower > current['lowestAsk']:
+       
+        elif last_sell_min * lower > current['lowestAsk']:
             rate=current['lowestAsk_orig']
-            amount_to_trade = min / int(rate.lstrip("0."))
+            amount_to_trade = unit / int(rate.lstrip("0."))
             try:
                 res = p.buy(currencyPair=exchange, rate=rate, amount=amount_to_trade, orderType="fillOrKill")
-                lib.showTrade(res, exchange, trade_type='buy', rate=rate, source='bot', amount=amount_to_trade)
+                lib.showTrade(res, exchange, trade_type='buy', rate=rate, source='bot2', amount=amount_to_trade)
             except Exception as ex:
-                print("  BUY  {:9} {:.3f} {}".format(exchange, 100 * (current['lowestAsk'] / last_rate - 1), ex))
+                print("  BUY  {:9} {:.3f} {}".format(exchange, 100 * (current['lowestAsk'] / last_sell_min - 1), ex))
+        
 
 
     print("------ {} ------".format(time.strftime("%Y-%m-%d %H:%M:%S")))
