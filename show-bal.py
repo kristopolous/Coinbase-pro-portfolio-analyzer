037036d66a52b3e62899cc2a6d07e18a184ef6e6
Chris McKenzie <kristopolous@yahoo.com>
2018-01-24 19:06:05 -0800

    fixing a few more bugs

--- show-bal.py
+++ show-bal.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import lib
+import sys
 from pprint import pprint
 
 all_history = lib.tradeHistory()
@@ -8,9 +9,10 @@ balanceMap = lib.returnCompleteBalances()
 for k,v in all_history.items():
     print(k)
 
+    #v = lib.analyze(v, currency=k, brief=True, sort='date')
     v = lib.analyze(v, currency=k, brief=True, sort='date')
+
     cur = k[4:]
     if cur in balanceMap:
         v['bal'] = balanceMap[cur]
-
     pprint(v)
